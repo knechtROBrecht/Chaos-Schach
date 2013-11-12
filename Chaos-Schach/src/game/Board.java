de28474be3154ba9fe291318026aaa6cc488a2ab
Robert <Robert.Bernhof@Haw-Hamburg.de>
2013-11-12 16:48:35 +0100

    fix for double painting in windows

--- Chaos-Schach/src/game/Board.java
+++ Chaos-Schach/src/game/Board.java
@@ -123,7 +123,7 @@ public class Board extends JPanel implements MouseListener, ActionListener,
 	}
 
 	public void paint(Graphics g) {
-
+		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
